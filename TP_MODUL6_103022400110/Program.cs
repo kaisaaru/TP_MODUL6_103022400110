581e546f09f27d1423ff766fa70fb5f3d743bc6c
Kaisar Rayfa Al Baihaqqi <ryf.kaisaaru@gmail.com>
2026-04-05 23:37:17 +0700

    add class SayaMusicTrack

--- TP_MODUL6_103022400110/Program.cs
+++ TP_MODUL6_103022400110/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MODUL6_103022400110;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of SayaMusicTrack and test the methods
+        SayaMusicTrack track = new SayaMusicTrack("My Favorite Song");
+        track.IncreasePlayCount(2);
+        track.PrintTrackDetails();
+    }
+}
---
+++ TP_MODUL6_103022400110/SayaMusicTrack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TP_MODUL6_103022400110
+{
+    class SayaMusicTrack
+    {
+        // Properties
+        private int id;
+        private string playCount;
+        private string title;
+
+        // Constructor
+        public SayaMusicTrack(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 100000); // 5 digit
+            this.playCount = "0";
+            this.title = title;
+        }
+
+        // Methods
+        public void IncreasePlayCount(int count)
+        {
+            // Convert playCount to integer, add count, and convert back to string
+            int currentPlayCount = int.Parse(playCount);
+            currentPlayCount += count;
+            playCount = currentPlayCount.ToString();
+        }
+
+        public void PrintTrackDetails()
+        {
+            // Print the track details
+            Console.WriteLine("Track Details:");
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+}
